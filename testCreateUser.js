d774fb23dfaa27d6e5a87375e30e3f724e31800e
lobintsev <a.lobintsev@sky-logic.ru>
2023-10-12 22:38:15 +0300

    update index.js, testCreateUser.js and testDeposit.js

--- testCreateUser.js
+++ testCreateUser.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 async function testCreateUser() {
     try {
         const response = await axios.post('http://localhost:3000/users', {
-            telegram_id: 123453445434343,
+            telegram_id: 12345543445434345453,
             first_name: 'John',
             last_name: 'Doe',
             telegram_nickname: 'john_doe'
