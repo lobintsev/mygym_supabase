42a8663bb3a668d62ad418f0af7eb7372e865e1e
lobintsev <a.lobintsev@sky-logic.ru>
2023-10-12 21:45:28 +0300

    update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/users', async (req, res) => {
 app.post('/users', async (req, res) => {
     const { telegram_id, first_name, last_name, telegram_nickname, phone } = req.body;  
 
-    if (!telegram_id || !first_name || !last_name || !telegram_nickname) {
+    if (!telegram_id || !first_name || !last_name ) {
         res.status(400).send('Bad Request: Missing required fields');
         return;
     }
