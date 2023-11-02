c8f55ef5d2b94b4e450a0b58115cbd767c99a487
Anton <a.lobintsev@sky-logic.ru>
2023-11-02 13:46:29 +0300

    update testCreateUser.js

--- testCreateUser.js
+++ testCreateUser.js
@@ -2,14 +2,14 @@ const axios = require('axios');
 
 async function testCreateUser() {
     try {
-        const response = await axios.post('http://localhost:3000/users', {
-            telegram_id: 12345344543434343,
+        const response = await axios.post('https://mygymapi.bebopbrands.com/users', {
+            telegram_id: 12345344543384343,
             first_name: 'John',
             last_name: 'Doe',
             telegram_nickname: 'john_doe',
             phone: '123-456-7890'
         });
-        console.log('User created successfully:', response.data);
+        console.log(response);
     } catch (error) {
         console.error('Error creating user:', error.response ? error.response.data : error.message);
     }
